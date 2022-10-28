4e208da9746f622bcb927fdff3b50afccb956e35
Francois Jacquier <fjacquier.externe@bedrockstreaming.com>
2022-10-28 14:46:06 +0200

    submit on enter

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import type { KeyboardEvent } from "react";
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "./styles";
 import { useFocus } from "./utils/useFocus";
 
@@ -10,9 +11,15 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
   const inputRef = useFocus();
 
+  const handleAddText = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text);
+    }
+  }
+
   return (
     <NewItemFormContainer>
-      <NewItemInput onChange={(e) => setText(e.target.value)} value={text} ref={inputRef} />
+      <NewItemInput onChange={(e) => setText(e.target.value)} value={text} ref={inputRef} onKeyDown={handleAddText} />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   );
